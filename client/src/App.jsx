497b2859872fe2e4f16557c1f353fcaa9eda2ff7
sandhya-1611 <141938427+sandhya-1611@users.noreply.github.com>
2025-08-20 14:18:38 +0530

    Setup Tailwind CSS with working test component

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen w-screen bg-white">
+      <h1 className="text-4xl font-bold text-blue-600">Hello Tailwind!</h1>
+    </div>
+  )
+}
+
+export default App
